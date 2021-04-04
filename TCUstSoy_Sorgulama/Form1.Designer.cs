6ce03b1b83306f0fb1eeed07e3dccc7026591388
hatcec <37028859+hatcec@users.noreply.github.com>
2021-04-04 14:23:11 +0300

    Proje dosyası ekle.

---
+++ TCUstSoy_Sorgulama/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace TCUstSoy_Sorgulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();          
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.TxtSoyad = new System.Windows.Forms.TextBox(); 
+            this.TxtTarih = new System.Windows.Forms.TextBox();
+           this.btn_Sorgula = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 13);
+            this.textBox1.MaxLength = 11;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Kimlik No Giriniz..";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(26, 39);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(100, 20);
+            this.TxtAd.TabIndex = 2;
+            this.TxtAd.Text = "İsim Giriniz..";
+            this.TxtAd.Enter += new System.EventHandler(this.TxtAd_Enter);
+            this.TxtAd.Leave += new System.EventHandler(this.TxtAd_Leave);
+           
+          
+           
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(26, 65);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(100, 20);
+            this.TxtSoyad.TabIndex = 4;
+            this.TxtSoyad.Text = "Soyisim Giriniz..";
+            this.TxtSoyad.Enter += new System.EventHandler(this.TxtSoyad_Enter);
+            this.TxtSoyad.Leave += new System.EventHandler(this.TxtSoyad_Leave);
+            // 
+            // TxtTarih
+            // 
+            this.TxtTarih.Location = new System.Drawing.Point(26, 91);
+            this.TxtTarih.MaxLength = 4;
+            this.TxtTarih.Name = "TxtTarih";
+            this.TxtTarih.Size = new System.Drawing.Size(100, 20);
+            this.TxtTarih.TabIndex = 3;
+            this.TxtTarih.Text = "Doğum Tarihi Giriniz..";
+            this.TxtTarih.Enter += new System.EventHandler(this.TxtTarih_Enter);
+            this.TxtTarih.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtTarih_KeyPress);
+            this.TxtTarih.Leave += new System.EventHandler(this.TxtTarih_Leave);
+            // 
+            // btn_Sorgula
+            // 
+            this.btn_Sorgula.Location = new System.Drawing.Point(26, 117);
+            this.btn_Sorgula.Name = "btn_Sorgula";
+            this.btn_Sorgula.Size = new System.Drawing.Size(101, 23);
+            this.btn_Sorgula.TabIndex = 1;
+            this.btn_Sorgula.Text = "TC No Sorgula";
+            this.btn_Sorgula.UseVisualStyleBackColor = true;
+            this.btn_Sorgula.Click += new System.EventHandler(this.btn_Sorgula_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.TxtTarih);
+            
+            this.Controls.Add(this.btn_Sorgula);
+           
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;      
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.TextBox TxtTarih;
+        private System.Windows.Forms.Button btn_Sorgula;
+    }
+}
+
---
+++ TCUstSoy_Sorgulama/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCUstSoy_Sorgulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        string sonuc;
+        string TCNO;
+        int on_sayi;
+        long TC;
+        public string TcKontrolUst(string TCNO)
+        {
+            String birinci = TCNO.Substring(0, 5);
+            String ikinci = TCNO.Substring(5, 4);
+            String ücüncü = TCNO.Substring(10, 1);
+            int kisim1 = Convert.ToInt32(birinci) + 3;
+            int kisim2 = Convert.ToInt32(ikinci) - 1;
+            int kisim3 = Convert.ToInt32(ücüncü);
+            if (birinci.Substring(2, 1) == kisim1.ToString().Substring(2, 1))
+            {
+                if (ikinci.Substring(1, 1) == kisim2.ToString().Substring(1, 1))
+                {
+                    if (kisim3 < 4)
+                    {
+                        kisim3 += 10;
+                        kisim3 -= 4;
+                    }
+                    else
+                    {
+                        kisim3 -= 4;
+                    }
+                }
+                else
+                {
+                    if (kisim3 < 4)
+                    {
+                        kisim3 += 10;
+                        kisim3 -= 2;
+                    }
+                    else
+                    {
+                        kisim3 -= 2;
+                    }
+                }
+            }
+            else
+            {
+                if (kisim3 < 4)
+                {
+                    kisim3 += 10;
+                    kisim3 -= 6;
+                }
+                else
+                {
+                    kisim3 -= 6;
+                }
+            }
+
+            int basamak1 = 0;
+            int toplam = 0;
+            int sayi1 = kisim1;
+            for (int i = 0; i < 5; i++)
+            {
+                basamak1 = sayi1 % 10;
+                sayi1 = sayi1 / 10;
+                toplam += basamak1;
+            }
+            int basamak2 = 0;
+            int sayi2 = kisim2;
+            for (int i = 0; i < 4; i++)
+            {
+                basamak2 = sayi2 % 10;
+                sayi2 = sayi2 / 10;
+                toplam += basamak2;
+            }
+            int a = Convert.ToInt32(toplam) % 10;
+            int on_sayi;
+            String sonuc;
+            if (a == kisim3)
+            {
+                on_sayi = 0;
+                sonuc = kisim1.ToString() + "" + kisim2.ToString() + "" + on_sayi.ToString() + "" + kisim3.ToString();
+            }
+            else
+            {
+                if (a < kisim3)
+                {
+                    on_sayi = kisim3 - a;
+                }
+                else
+                {
+                    on_sayi = a - kisim3;
+                }
+                sonuc = kisim1.ToString() + "" + kisim2.ToString() + "" + on_sayi.ToString() + "" + kisim3.ToString();
+            }
+            if (kisim3 % 2 != 0)
+            {
+                return TcKontrolUst(sonuc);
+            }
+            else
+            {
+                return sonuc;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Kimlik Numarası Giriniz..";
+                textBox1.ForeColor = Color.Silver;
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Kimlik Numarası Giriniz..")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void TxtAd_Enter(object sender, EventArgs e)
+        {
+            if (TxtAd.Text == "İsim Giriniz..")
+            {
+                TxtAd.Text = "";
+                TxtAd.ForeColor = Color.Black;
+            }
+        }
+
+        private void TxtAd_Leave(object sender, EventArgs e)
+        {
+            if (TxtAd.Text == "")
+            {
+                TxtAd.Text = "İsim Giriniz..";
+                TxtAd.ForeColor = Color.Silver;
+            }
+        }
+
+        private void TxtSoyad_Enter(object sender, EventArgs e)
+        {
+            if (TxtSoyad.Text == "Soyisim Giriniz..")
+            {
+                TxtSoyad.Text = "";
+                TxtSoyad.ForeColor = Color.Black;
+            }
+        }
+
+        private void TxtSoyad_Leave(object sender, EventArgs e)
+        {
+            if (TxtSoyad.Text == "")
+            {
+                TxtSoyad.Text = "Soyisim Giriniz..";
+                TxtSoyad.ForeColor = Color.Silver;
+            }
+        }
+
+        private void TxtTarih_Enter(object sender, EventArgs e)
+        {
+            if (TxtTarih.Text == "Doğum Tarihi Giriniz..")
+            {
+                TxtTarih.Text = "";
+                TxtTarih.ForeColor = Color.Black;
+            }
+        }
+
+        private void TxtTarih_Leave(object sender, EventArgs e)
+        {
+            if (TxtTarih.Text == "")
+            {
+                TxtTarih.Text = "Doğum Tarihi Giriniz..";
+                TxtTarih.ForeColor = Color.Silver;
+            }
+        }
+
+        private void TxtTarih_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void btn_Sorgula_Click(object sender, EventArgs e)
+        {
+            TCNO = textBox1.Text;
+            string kimlikno = TcKontrolUst(TCNO);
+            bool durum = false;
+            while (durum != true)
+            {
+                using (KimlikSorgulama.KPSPublicSoapClient service = new KimlikSorgulama.KPSPublicSoapClient())
+                {
+                    TC = Convert.ToInt64(kimlikno);
+                    durum = service.TCKimlikNoDogrula(TC, TxtAd.Text.ToUpper(), TxtSoyad.Text.ToUpper(), Convert.ToInt32(TxtTarih.Text));
+                    string d = TC.ToString();
+                    kimlikno = TcKontrolUst(d);
+                }
+            }
+            string F = TC.ToString();
+            MessageBox.Show(durum.ToString() + "-- TC Kimlik Numarası:" + F);
+        }
+
+    }
+}
